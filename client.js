8126f09feae4c44a8b0051bd6a2dd96d8da0a92c
Georgina Bada <georginabada@gmail.com>
2021-12-01 14:14:56 -0500

    Exporting connect from play.js to client.js

---
+++ client.js
@@ -0,0 +1,19 @@
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',// IP address here,
+    port: 50541,// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  
+  conn.on('data',function(message){
+    console.log(message);
+  });
+
+  return conn;
+
+};
+
+module.exports = connect;
